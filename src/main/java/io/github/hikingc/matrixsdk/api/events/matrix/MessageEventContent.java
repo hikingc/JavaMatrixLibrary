e44469482389a9cbf58af982302dd85badf2079b
J.C <73438877+1hiking@users.noreply.github.com.>
2026-08-13 18:49:01 -0600

    Fix wrong event type being added

--- src/main/java/io/github/hikingc/matrixsdk/api/events/matrix/MessageEventContent.java
+++ src/main/java/io/github/hikingc/matrixsdk/api/events/matrix/MessageEventContent.java
@@ -4,8 +4,24 @@ import io.github.hikingc.matrixsdk.api.events.matrix.call.*;
 import io.github.hikingc.matrixsdk.api.events.matrix.key.*;
 import io.github.hikingc.matrixsdk.api.events.matrix.room.RoomMessage;
 import io.github.hikingc.matrixsdk.api.events.matrix.room.RoomRedaction;
-import io.github.hikingc.matrixsdk.api.events.server.message.KeyVerificationRequestEvent;
 
 /// Marker interface for input message events.
 public sealed interface MessageEventContent
-        permits Reaction, Sticker, CallAnswer, CallCandidates, CallHangup, CallInvite, CallNegotiate, CallReject, CallSelectAnswer, KeyVerificationAccept, KeyVerificationCancel, KeyVerificationDone, KeyVerificationKey, KeyVerificationMac, KeyVerificationRequest, KeyVerificationStart, RoomMessage, RoomRedaction, KeyVerificationRequestEvent {}
+    permits Reaction,
+        Sticker,
+        CallAnswer,
+        CallCandidates,
+        CallHangup,
+        CallInvite,
+        CallNegotiate,
+        CallReject,
+        CallSelectAnswer,
+        KeyVerificationAccept,
+        KeyVerificationCancel,
+        KeyVerificationDone,
+        KeyVerificationKey,
+        KeyVerificationMac,
+        KeyVerificationRequest,
+        KeyVerificationStart,
+        RoomMessage,
+        RoomRedaction {}
