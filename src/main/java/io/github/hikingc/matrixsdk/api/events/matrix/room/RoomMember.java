4b80d42ea106a17f59e41c543783f546bd50c358
J.C <73438877+1hiking@users.noreply.github.com.>
2026-08-14 17:27:16 -0600

    RoomMember was given @Nullable annotation

--- src/main/java/io/github/hikingc/matrixsdk/api/events/matrix/room/RoomMember.java
+++ src/main/java/io/github/hikingc/matrixsdk/api/events/matrix/room/RoomMember.java
@@ -2,17 +2,17 @@ package io.github.hikingc.matrixsdk.api.events.matrix.room;
 
 import io.github.hikingc.matrixsdk.api.events.ThirdPartyInvite;
 import io.github.hikingc.matrixsdk.api.events.matrix.StateEventContent;
+import io.github.hikingc.matrixsdk.api.events.queries.Membership;
+import org.jspecify.annotations.Nullable;
 
 import java.net.URI;
 
 public record RoomMember(
     URI avatarUrl,
-    String displayname, // according to spec its either String or Null, should it be treated
-    // differently? - 31/jul/2026
+    @Nullable String displayname,
     Boolean isDirect,
     String joinAuthorizedViaUsersServer,
-    String membership,
+    Membership membership,
     String reason,
     ThirdPartyInvite thirdPartyInvite)
     implements StateEventContent {}
-// displayname is written without camel-case style
