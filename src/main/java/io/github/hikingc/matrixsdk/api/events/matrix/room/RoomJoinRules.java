02dd8f7fc98fc7e9997092508b3c9a49fba45d7a
J.C <73438877+1hiking@users.noreply.github.com.>
2026-08-14 17:26:49 -0600

    RoomJoinRules added missing List type

--- src/main/java/io/github/hikingc/matrixsdk/api/events/matrix/room/RoomJoinRules.java
+++ src/main/java/io/github/hikingc/matrixsdk/api/events/matrix/room/RoomJoinRules.java
@@ -5,8 +5,10 @@ import io.github.hikingc.matrixsdk.api.events.matrix.StateEventContent;
 import io.github.hikingc.matrixsdk.api.identifiers.RoomID;
 import org.jspecify.annotations.NonNull;
 
+import java.util.List;
+
 public record RoomJoinRules(
-    AllowCondition allow, @NonNull @JsonProperty(required = true) String joinRule)
+        List<AllowCondition> allow, @NonNull @JsonProperty(required = true) String joinRule)
     implements StateEventContent {
 
   public record AllowCondition(
