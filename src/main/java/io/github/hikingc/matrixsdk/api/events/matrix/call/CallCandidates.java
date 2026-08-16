239c3b6e11d4150343ca56357baf03f153a46794
J.C <73438877+1hiking@users.noreply.github.com.>
2026-08-16 10:24:34 -0600

    CallCandidates: Fix type (should be a List)

--- src/main/java/io/github/hikingc/matrixsdk/api/events/matrix/call/CallCandidates.java
+++ src/main/java/io/github/hikingc/matrixsdk/api/events/matrix/call/CallCandidates.java
@@ -4,9 +4,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.github.hikingc.matrixsdk.api.events.matrix.MessageEventContent;
 import org.jspecify.annotations.NonNull;
 
+import java.util.List;
+
 public record CallCandidates(
     @NonNull @JsonProperty(required = true) String callId,
-    @NonNull @JsonProperty(required = true) Candidate candidates,
+    @NonNull @JsonProperty(required = true) List<Candidate> candidates,
     @NonNull @JsonProperty(required = true) String partyId,
     @NonNull @JsonProperty(required = true) String version)
     implements MessageEventContent {
