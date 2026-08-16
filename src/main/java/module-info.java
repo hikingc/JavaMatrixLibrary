2ed58d124696c1cc83eab54f999e9fb932c8e647
J.C <73438877+1hiking@users.noreply.github.com.>
2026-08-15 20:42:59 -0600

    module-info.java: Add missing export and organize a bit the rest.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -11,6 +11,8 @@ module io.github.hikingc.matrixsdk {
   // Required for JSON manipulation
   requires tools.jackson.core;
   requires tools.jackson.databind;
+  exports io.github.hikingc.matrixsdk.services.utils.deserializers to tools.jackson.databind;
+
 
   // Logging
   requires org.slf4j;
@@ -24,24 +26,31 @@ module io.github.hikingc.matrixsdk {
   exports io.github.hikingc.matrixsdk.context;
 
   // Records and Interfaces
-  exports io.github.hikingc.matrixsdk.api.rooms;
   exports io.github.hikingc.matrixsdk.api.events;
   exports io.github.hikingc.matrixsdk.api.events.server;
   exports io.github.hikingc.matrixsdk.api.events.server.message;
   exports io.github.hikingc.matrixsdk.api.events.server.state;
+  exports io.github.hikingc.matrixsdk.api.events.server.ephemeral;
+
   exports io.github.hikingc.matrixsdk.api.events.matrix.ephemeral;
   exports io.github.hikingc.matrixsdk.api.events.matrix;
   exports io.github.hikingc.matrixsdk.api.events.matrix.room.messages;
   exports io.github.hikingc.matrixsdk.api.events.matrix.room;
   exports io.github.hikingc.matrixsdk.api.events.matrix.call;
   exports io.github.hikingc.matrixsdk.api.events.matrix.space;
+
   exports io.github.hikingc.matrixsdk.api.events.queries;
   exports io.github.hikingc.matrixsdk.api.events.sync;
   exports io.github.hikingc.matrixsdk.api.events.crypto;
+
+  exports io.github.hikingc.matrixsdk.api.rooms;
   exports io.github.hikingc.matrixsdk.api.rooms.queries;
   exports io.github.hikingc.matrixsdk.api.rooms.models;
+
   exports io.github.hikingc.matrixsdk.api.userdata;
+
   exports io.github.hikingc.matrixsdk.api.auth;
+
   exports io.github.hikingc.matrixsdk.api.filters;
 
   // Exceptions
