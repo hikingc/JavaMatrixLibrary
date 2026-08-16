bc69a3dbbe8e7eed2d0c6ca66970867ea1bd3e46
J.C <73438877+1hiking@users.noreply.github.com.>
2026-08-16 13:40:22 -0600

    Reaction: Patch wrong object structure

--- src/main/java/io/github/hikingc/matrixsdk/api/events/matrix/Reaction.java
+++ src/main/java/io/github/hikingc/matrixsdk/api/events/matrix/Reaction.java
@@ -1,6 +1,17 @@
 package io.github.hikingc.matrixsdk.api.events.matrix;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.github.hikingc.matrixsdk.api.events.matrix.room.RoomRedaction;
+import io.github.hikingc.matrixsdk.api.identifiers.EventID;
 
-public record Reaction(@JsonProperty("m.relates_to") String mRelatesTo)
-    implements MessageEventContent {}
+public record Reaction(@JsonProperty("m.relates_to") ReactionRelatesTo mRelatesTo)
+    implements MessageEventContent {
+  public record ReactionRelatesTo(EventID eventId,
+                                  String key) {
+    @JsonGetter("relType")
+    public String getRelType() {
+      return "m.reaction";
+    }
+  }
+}
