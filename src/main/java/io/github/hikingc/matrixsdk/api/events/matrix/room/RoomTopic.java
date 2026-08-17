f1c2a1c2f5e1ecb59766f5663adb489b111011df
J.C <73438877+1hiking@users.noreply.github.com.>
2026-08-17 12:18:33 -0600

    RoomTopic: Fix model

--- src/main/java/io/github/hikingc/matrixsdk/api/events/matrix/room/RoomTopic.java
+++ src/main/java/io/github/hikingc/matrixsdk/api/events/matrix/room/RoomTopic.java
@@ -2,10 +2,12 @@ package io.github.hikingc.matrixsdk.api.events.matrix.room;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.github.hikingc.matrixsdk.api.events.matrix.StateEventContent;
-
 import java.util.List;
+import org.jspecify.annotations.NonNull;
 
-public record RoomTopic(@JsonProperty("m.topic") TopicContentBlock mTopic)
+public record RoomTopic(
+    @JsonProperty("m.topic") TopicContentBlock mTopic,
+    @NonNull @JsonProperty(required = true) String topic)
     implements StateEventContent {
 
   public record TopicContentBlock(@JsonProperty("m.text") List<TextualRepresentation> mText) {
