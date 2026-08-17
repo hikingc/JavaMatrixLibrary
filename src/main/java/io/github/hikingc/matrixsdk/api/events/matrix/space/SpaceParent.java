897db1c56f509f4af0f75184c67359214b9564bb
J.C <73438877+1hiking@users.noreply.github.com.>
2026-08-16 19:26:15 -0600

    SpaceParent: Remove property requirement.

--- src/main/java/io/github/hikingc/matrixsdk/api/events/matrix/space/SpaceParent.java
+++ src/main/java/io/github/hikingc/matrixsdk/api/events/matrix/space/SpaceParent.java
@@ -1,10 +1,6 @@
 package io.github.hikingc.matrixsdk.api.events.matrix.space;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import io.github.hikingc.matrixsdk.api.events.matrix.StateEventContent;
 import java.util.List;
-import org.jspecify.annotations.NonNull;
 
-public record SpaceParent(
-    Boolean canonical, @NonNull @JsonProperty(required = true) List<String> via)
-    implements StateEventContent {}
+public record SpaceParent(Boolean canonical, List<String> via) implements StateEventContent {}
