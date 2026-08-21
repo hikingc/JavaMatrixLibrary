ac1c75992f921bf9f071fcf944083015cb7c19da
J.C <73438877+1hiking@users.noreply.github.com.>
2026-08-21 16:38:33 -0600

    Key: Patch missing jsontypenames

--- src/main/java/io/github/hikingc/matrixsdk/api/events/server/message/KeyVerificationCancelEvent.java
+++ src/main/java/io/github/hikingc/matrixsdk/api/events/server/message/KeyVerificationCancelEvent.java
@@ -1,5 +1,6 @@
 package io.github.hikingc.matrixsdk.api.events.server.message;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import io.github.hikingc.matrixsdk.api.events.MessageEvent;
 import io.github.hikingc.matrixsdk.api.events.UnsignedData;
 import io.github.hikingc.matrixsdk.api.events.matrix.key.KeyVerificationCancel;
@@ -7,6 +8,7 @@ import io.github.hikingc.matrixsdk.api.identifiers.EventID;
 import io.github.hikingc.matrixsdk.api.identifiers.RoomID;
 import io.github.hikingc.matrixsdk.api.identifiers.UserID;
 
+@JsonTypeName("m.key.verification.cancel")
 public record KeyVerificationCancelEvent(
     KeyVerificationCancel content,
     EventID eventId,
--- src/main/java/io/github/hikingc/matrixsdk/api/events/server/message/KeyVerificationStartEvent.java
+++ src/main/java/io/github/hikingc/matrixsdk/api/events/server/message/KeyVerificationStartEvent.java
@@ -1,5 +1,6 @@
 package io.github.hikingc.matrixsdk.api.events.server.message;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import io.github.hikingc.matrixsdk.api.events.MessageEvent;
 import io.github.hikingc.matrixsdk.api.events.UnsignedData;
 import io.github.hikingc.matrixsdk.api.events.matrix.key.KeyVerificationStart;
@@ -7,6 +8,7 @@ import io.github.hikingc.matrixsdk.api.identifiers.EventID;
 import io.github.hikingc.matrixsdk.api.identifiers.RoomID;
 import io.github.hikingc.matrixsdk.api.identifiers.UserID;
 
+@JsonTypeName("m.key.verification.start")
 public record KeyVerificationStartEvent(
     KeyVerificationStart content,
     EventID eventId,
