bc8b9f47984075f0be1047d3e8bc111f75b9ddc9
J.C <73438877+1hiking@users.noreply.github.com.>
2026-08-23 21:26:24 -0600

    Format list

--- src/main/java/io/github/hikingc/matrixsdk/api/events/matrix/EphemeralContent.java
+++ src/main/java/io/github/hikingc/matrixsdk/api/events/matrix/EphemeralContent.java
@@ -2,4 +2,10 @@ package io.github.hikingc.matrixsdk.api.events.matrix;
 
 import io.github.hikingc.matrixsdk.api.events.matrix.ephemeral.*;
 
-public sealed interface EphemeralContent permits EphemeralDirect, EphemeralFullyRead, EphemeralPresence, EphemeralReceipt, EphemeralTag, EphemeralTyping {}
+public sealed interface EphemeralContent
+    permits EphemeralDirect,
+        EphemeralFullyRead,
+        EphemeralPresence,
+        EphemeralReceipt,
+        EphemeralTag,
+        EphemeralTyping {}
