be7a7ee56d91a2caef3f12ed7a2139b3a1dbfc4f
J.C <73438877+1hiking@users.noreply.github.com.>
2026-09-02 14:42:22 -0600

    EphemeralEvent: Cleanup documentation regarding ephemeral events.

--- src/main/java/io/github/hikingc/matrixsdk/api/events/EphemeralEvent.java
+++ src/main/java/io/github/hikingc/matrixsdk/api/events/EphemeralEvent.java
@@ -5,19 +5,24 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import io.github.hikingc.matrixsdk.api.events.matrix.EphemeralContent;
 import io.github.hikingc.matrixsdk.api.events.server.ephemeral.*;
 
-/// A minimal event with no room, sender, or metadata context — used for account data, presence, and
-/// to-device events.
+/// A minimal event with no room, sender, or metadata context. The Matrix specification returns
+/// these events at [io.github.hikingc.matrixsdk.api.events.sync.Sync]
+///
+/// Filtering ephemeral events is possible with the use of
+/// [filters][io.github.hikingc.matrixsdk.api.Filter] using [filter
+/// definitions][io.github.hikingc.matrixsdk.api.filters.FilterDefinition#room()]
 ///
 /// @param <T> the `m.` type event.
 @JsonInclude(JsonInclude.Include.NON_ABSENT)
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
-public sealed interface EphemeralEvent<T extends EphemeralContent> permits DirectEvent, FullyReadEvent, PresenceEvent, ReceiptEvent, TagEvent, TypingEvent {
-  /// the event content, shape depends on `type`.
+public sealed interface EphemeralEvent<T extends EphemeralContent>
+    permits DirectEvent, FullyReadEvent, PresenceEvent, ReceiptEvent, TagEvent, TypingEvent {
+  /// The event content, shape depends on `type`.
   ///
   /// @return the event content.
   T content();
 
-  /// the event type, for example, `m.typing`.
+  /// The event type, for example, `m.typing`.
   ///
   /// @return the event type.
   String type();
