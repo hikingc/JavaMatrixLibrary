2088574bcf8e53a3b7b6f7687b16733f08836a87
J.C <73438877+1hiking@users.noreply.github.com.>
2026-09-13 16:49:31 -0600

    (Docs) Update exception throws

--- matrix-sdk-core/src/main/java/io/github/hikingc/matrixsdk/api/Filter.java
+++ matrix-sdk-core/src/main/java/io/github/hikingc/matrixsdk/api/Filter.java
@@ -5,7 +5,6 @@ import io.github.hikingc.matrixsdk.api.identifiers.UserID;
 import io.github.hikingc.matrixsdk.exceptions.MatrixApiException;
 import io.github.hikingc.matrixsdk.exceptions.MatrixException;
 
-
 /// Core interface for executing protocol operations for filtering.
 ///
 /// All operations in this interface are blocking. Implementations must ensure thread safety and
@@ -15,22 +14,22 @@ import io.github.hikingc.matrixsdk.exceptions.MatrixException;
 ///   API Specification for Filters</a>
 public interface Filter {
 
-    /// Uploads a new filter definition to the homeserver. Returns a filter ID that may be used in
-    /// future requests to restrict which events are returned to the client.
-    ///
-    /// @param userId the [UserID] of whoever is uploading the server.
-    /// @param filter the definition of the filter.
-    /// @return an ID of the filter definition, usable in supported endpoints.
-    /// @throws MatrixException    if an error occurred while processing.
-    /// @throws MatrixApiException if the server returns an unsuccessful answer.
-    String publishFilter(UserID userId, FilterDefinition filter);
+  /// Uploads a new filter definition to the homeserver. Returns a filter ID that may be used in
+  /// future requests to restrict which events are returned to the client.
+  ///
+  /// @param userId the [UserID] of whoever is uploading the server.
+  /// @param filter the definition of the filter.
+  /// @return an ID of the filter definition, usable in supported endpoints.
+  /// @throws MatrixException    if an error occurred while processing.
+  /// @throws MatrixApiException if the server returns an unsuccessful answer.
+  String publishFilter(UserID userId, FilterDefinition filter);
 
-    /// Retrieve a [FilterDefinition] from the homeserver.
-    ///
-    /// @param userId   the [UserID] to download a filter for.
-    /// @param filterId the filter ID to download.
-    /// @return a [FilterDefinition] with all uploaded data.
-    /// @throws MatrixException    if an error occurred while processing.
-    /// @throws MatrixApiException if the server returns an unsuccessful answer.
-    FilterDefinition getFilter(UserID userId, String filterId);
+  /// Retrieve a [FilterDefinition] from the homeserver.
+  ///
+  /// @param userId   the [UserID] to download a filter for.
+  /// @param filterId the filter ID to download.
+  /// @return a [FilterDefinition] with all uploaded data.
+  /// @throws MatrixException    if an error occurred while processing.
+  /// @throws MatrixApiException if the server returns an unsuccessful answer.
+  FilterDefinition getFilter(UserID userId, String filterId);
 }
